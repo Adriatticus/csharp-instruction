e923a72d84f7ffbf5e432db9a58e2305468b3be1
Adriatticus <4CorderJ@gmail.com>
2025-02-05 08:10:02 -0500

    a little progress on project 5-3

--- csharp-project-5-3-guessing-game/Program.cs
+++ csharp-project-5-3-guessing-game/Program.cs
@@ -11,6 +11,36 @@ namespace csharp_project_5_3_guessing_game
             Print("\nWelcome to the Guess the Number Game");
             Print("++++++++++++++++++++++++++++++++++++\n");
 
+            string choice = "y";
+            while (choice.ToLower() == "y")
+            {
+
+
+                Print("I'm thinking of a number from 1 to 100.\nTry to guess it.");
+                Random ranNum = new Random();
+                Console.Write(ranNum.Next(0, 100));
+                bool successvalidguess = false;
+                while (!successvalidguess)
+                {
+                    Print("\nEnter number: ");
+                    int guess = Int32.Parse(Console.ReadLine());
+                    if (guess < 1 || guess > 100)
+                    {
+                        Console.Write("\nInvalid guess. Try again: ");
+                    }
+                    else
+                    {
+                        successvalidguess = true;
+                    }
+                }
+
+            Console.Write("\nTry again? (y/n): ");
+            choice = Console.ReadLine();
+            Print(" ");
+            }
+
+
+
             Print("\nNope! My turn!");
         }
 
