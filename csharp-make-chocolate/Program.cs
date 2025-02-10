423dafc5a06bc0060a33449bfdd4ce9dae481c75
Adriatticus <4CorderJ@gmail.com>
2025-02-10 04:27:14 -0500

    Chocolate arrows

---
+++ csharp-make-chocolate/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace csharp_make_chocolate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chocolate Time!!");
+
+
+            Console.Write("makeChocolate(4, 1, 9) -> ");
+            Console.WriteLine(makeChocolate(4, 1, 9).ToString());
+
+            Console.Write("makeChocolate(4, 1, 9) -> ");
+            Console.WriteLine(makeChocolate(4, 1, 10).ToString());
+
+            Console.Write("makeChocolate(4, 1, 9) -> ");
+            Console.WriteLine(makeChocolate(4, 1, 7).ToString());
+
+
+
+            Console.WriteLine("Yum!");
+        }
+        public static int makeChocolate(int small, int big, int goal)
+        {
+            int remaining = goal % 5;
+            if ((goal > (big * 5 + small)) || (remaining > small)) 
+            {
+                return -1;
+            }
+            else
+            {
+                return remaining;
+            }
+            return 0;
+
+        }
+
+    }
+}
---
+++ csharp-the-arrow/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.InteropServices;
+
+namespace csharp_the_arrow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simons bow Outfitters\n");
+
+            int size = 0;
+            Console.Write("How many arrows will you need for todays hunt? ");
+            bool success = int.TryParse(Console.ReadLine(), out size);
+            Console.WriteLine("");
+            if (!success)
+            {
+                Console.WriteLine("Wrong!");
+            }
+            else
+            {
+                printArrowHead(size);
+            }
+
+
+            Console.WriteLine("\nHappy hunting!");
+        }
+        public static void printArrowHead(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(">");
+                }
+                Console.WriteLine();
+
+
+            }
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(">");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
