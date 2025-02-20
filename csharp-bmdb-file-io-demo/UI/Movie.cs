ca25086e4e35433cb00f1752208f5e62c4cc181c
adrian-johnson <4corderoj@gmail.com>
2025-02-20 16:44:01 -0500

    api intro movie db stuff

---
+++ csharp-bmdb-file-io-demo/UI/Movie.cs
@@ -0,0 +1,26 @@
+﻿namespace csharp_bmdb_file_io_demo.UI
+{
+    internal class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Rating { get; set; }
+        public string Director { get; set; }
+
+
+        public Movie(int id, string title, int year, string rating, string director)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Year = year;
+            this.Rating = rating;
+            this.Director = director;
+        }
+
+        public override string ToString()
+        {
+            return $"Movie: ({Id}) - {Title}, {Year}, {Rating}, {Director}";
+        }
+    }
+}
---
+++ csharp-bmdb-file-io-demo/UI/Program.cs
@@ -0,0 +1,27 @@
+﻿using ConsoleLibrary;
+
+namespace csharp_bmdb_file_io_demo.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyConsole.PrintL("Hello dbdbdbs");
+
+            //Movie m1 = new Movie(1, "Spaceballs", 1985, "PG", "Mel Brooks");
+            //Movie m2 = new Movie(2, "Totally Awesome", 2025, "PG-13", "Sean Blessing");
+
+            List<Movie> movies = new List<Movie>();
+            //movies.Add(m1);
+            //movies.Add(m2);
+            //MovieDB.SaveMovies(movies);
+
+            movies = MovieDB.GetMovies();
+            foreach (Movie movie in movies)
+            {
+                MyConsole.PrintL(movie.ToString());
+            }
+
+        }
+    }
+}
